66d167783643ff27cb8bf65e6ed92693bd228f93
ashutosh1997 <kumar.ashutosh920@gmail.com>
2017-01-04 05:30:58 +0530

    Done

--- webCrawler.py
+++ webCrawler.py
@@ -1,4 +1,32 @@
-import urllib
+import requests
 from bs4 import BeautifulSoup
 
+
+def crawler(max_pages):
+    page = 1
+    while page < max_pages:
+        url = "https://ultimatenotes.herokuapp.com/semester"+str(page)+".html"
+        source_code = requests.get(url)
+        plain_text = source_code.text
+        soup = BeautifulSoup(plain_text, "html.parser")
+        for link in soup.findAll('a', {'class': 'item-name'}):
+            href = 'https://ultimatenotes.herokuapp.com/'+link.get('href')
+            title = link.string
+            print(href)
+            print(title)
+        page += 1
+
+
+def crawler2(max_pages):
+    page = 1
+    while page < max_pages:
+        url = "https://www.flipkart.com/"
+        source_code = requests.get(url)
+        plain_text = source_code.text
+        soup = BeautifulSoup(plain_text, "html.parser")
+        title = soup.string
+        print(title)
+        page += 1
+
+crawler2(5)
 
